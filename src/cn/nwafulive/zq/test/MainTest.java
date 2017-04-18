7f5c606c3d16ce1e2c44915b4ed0286f8fa036c4
nwsuafzq <804194244@qq.com>
2017-04-18 16:02:57 +0800

    增添了一些测试代码文本

--- src/cn/nwafulive/zq/test/MainTest.java
+++ src/cn/nwafulive/zq/test/MainTest.java
@@ -13,23 +13,21 @@ public class MainTest {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO 自动生成的方法存根
-		File file = new File(".\\programme\\p.txt");
-	    FileInputStream in = new FileInputStream(file);
-	    SimpleCharStream scstream = new SimpleCharStream(in);
-	    ParserTokenManager tm = new ParserTokenManager(scstream);
-		Parser parser = new Parser(tm);	 
-		try
-	    {
-	      SimpleNode n = parser.Start();
-	      n.dump("");
-	      parser.qtTable.printQTTable();
-	      System.out.println("Thank you.");
-	    }
-	    catch (Exception e)
-	    {
-	      System.out.println("Oops.");
-	      System.out.println(e.getMessage());
-	    }
+		File file = new File(".\\programme\\whilecode.txt");
+		FileInputStream in = new FileInputStream(file);
+		SimpleCharStream scstream = new SimpleCharStream(in);
+		ParserTokenManager tm = new ParserTokenManager(scstream);
+		Parser parser = new Parser(tm);
+		try {
+
+			SimpleNode n = parser.Start();
+			n.dump("");
+			parser.qtTable.printQTTable();
+			System.out.println("Thank you.");
+		} catch (Exception e) {
+			System.out.println("Oops.");
+			System.out.println(e.getMessage());
+		}
 
 	}
 
